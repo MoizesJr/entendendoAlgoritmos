85ef49e3b3bce62589bc4b0fc6a69e29f7887941
Moizés Júnior <128239325+MoizesJr@users.noreply.github.com>
2025-06-14 07:12:02 -0300

    teste fateando string

--- src/App.java
+++ src/App.java
@@ -1,15 +1,15 @@
 public class App {
+
+    public static void regressiva(int i) {
+        System.out.println(i);
+        if (i <= 1) {
+            return;
+        } else {
+            regressiva(i - 1);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        String str = "Cuida menino";
-
-        int totalString = str.length();
-        char primeiro = str.charAt(0);
-        char segunda = str.charAt(1);
-        char ultimo = str.charAt(str.length() - 1);
-
-        System.out.println(primeiro);
-        System.out.println(segunda);
-        System.out.println(totalString);
-        System.out.println(ultimo);
+        regressiva(3);
     }
 }
